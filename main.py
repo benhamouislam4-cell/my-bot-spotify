4f9790e5877c1ae3e33a306f18423baea08ef020
benhamouislam4-cell <benhamouislam4@gmail.com>
2026-03-24 14:46:40 +0100

    Update bot token and improve audio download function

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiohttp import web
 import yt_dlp
 
 # --- الإعدادات ---
-API_TOKEN = 'ضع_توكن_البوت_الجديد_هنا' 
+API_TOKEN = '8764560809:AAGIP_1z_xFZv5TMezYEemYSZTnI5P-VznY' 
 CHANNEL_ID = '@Ramy_Premium'
 CHANNEL_LINK = 'https://t.me/Ramy_Premium'
 
